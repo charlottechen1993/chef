f4c56b9d158006d4241d4a65ea7828c2c0d8678a
Charlotte Chen <charlottechen21@gmail.com>
2016-09-12 16:57:32 -0400

    color

--- js/myctrl.js
+++ js/myctrl.js
@@ -3,24 +3,35 @@ app.controller("recipesCtrl", function($scope) {
         {
             Title: "Bibimbap",
             Description: "This is very tasty",
-            Primary: "Asian",
-            Secondary: "Rice",
+            Primary: "Dishes",
+            SecondTitle: "Korean",
             Image: "bibimbap.jpg",
-            Count: 2
+            Count: 2,
+            LastCooked: "2015-08-10"
         },{
             Title: "Classic burger",
             Description: "Very rich in flavor",
-            Primary: "American",
-            Secondary: "Burgers",
+            Primary: "Dishes",
+            SecondTitle: "American",
             Image: "burger.jpeg",
-            Count: 8
+            Count: 8,
+            LastCooked: "2016-07-10"
         },{
             Title: "Chinese Fried Rice",
             Description: "Good for lunch and dinner",
-            Primary: "Asian",
-            Secondary: "Rice",
+            Primary: "Dishes",
+            SecondTitle: "Chinese",
             Image: "fried-rice.jpg",
-            Count: 1
+            Count: 1,
+            LastCooked: "2016-06-18"
+        },{
+            Title: "Chinese Fried Rice",
+            Description: "Good for lunch and dinner",
+            Primary: "Dishes",
+            SecondTitle: "Chinese",
+            Image: "fried-rice.jpg",
+            Count: 1,
+            LastCooked: "2016-08-10"
         }
     ]
     
---
+++ js/secondary.js
@@ -0,0 +1,14 @@
+app.controller("secondaryCtrl", function($scope) {
+    $scope.secondType = [
+        {
+            secondTitle: "Chinese",
+            Number: 2
+        },{
+            secondTitle: "American",
+            Number: 1
+        },{
+            secondTitle: "Korean",
+            Number: 1
+        },
+    ]
+});
