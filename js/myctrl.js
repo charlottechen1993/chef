f0ae052d6d16a5a31d574b36b05a0200a1ae6c48
Charlotte Chen <charlottechen21@gmail.com>
2016-09-12 11:29:40 +0800

    debugging selector

--- js/myctrl.js
+++ js/myctrl.js
@@ -1,26 +1,26 @@
 app.controller("recipesCtrl", function($scope) {
     $scope.recipes = [
         {
-            "Title": "Bibimbap",
-            "Description": "This is very tasty",
-            "Primary-Type": "Asian",
-            "Secondary-Type": "Rice",
-            "Image": "bibimbap.jpg",
-            "Count": 2
+            Title: "Bibimbap",
+            Description: "This is very tasty",
+            Primary: "Asian",
+            Secondary: "Rice",
+            Image: "bibimbap.jpg",
+            Count: 2
         },{
-            "Title": "Classic burger",
-            "Description": "Very rich in flavor",
-            "Primary-Type": "American",
-            "Secondary-Type": "Burgers",
-            "Image": "burger.jpeg",
-            "Count": 3
+            Title: "Classic burger",
+            Description: "Very rich in flavor",
+            Primary: "American",
+            Secondary: "Burgers",
+            Image: "burger.jpeg",
+            Count: 8
         },{
-            "Title": "Chinese Fried Rice",
-            "Description": "Good for lunch and dinner",
-            "Primary-Type": "Asian",
-            "Secondary-Type": "Rice",
-            "Image": "fried-rice.jpg",
-            "Count": 4
+            Title: "Chinese Fried Rice",
+            Description: "Good for lunch and dinner",
+            Primary: "Asian",
+            Secondary: "Rice",
+            Image: "fried-rice.jpg",
+            Count: 4
         }
     ]
 });
